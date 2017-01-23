db1592c87f8d3c1d497aa34f3a1fa74c84fdb4c6
Marc Waldthausen <marc.waldthausen@gmail.com>
2017-01-23 06:41:04 -0700

    Updated for round 2.

--- src/github.com/mwaldtha/pwdsrv/password_server.go
+++ src/github.com/mwaldtha/pwdsrv/password_server.go
@@ -1,63 +1,221 @@
-package main 
+package main
 
 import (
-    "crypto/sha512"
-    "encoding/base64"
-    "flag"
-    "fmt"
-    "log"
-    "net/http"
-    "time"
+	"crypto/sha512"
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"syscall"
+	"time"
 )
 
 const (
-    postMethod string = "POST"
-    passwordFormName string = "password"
+	passwordFormName string = "password"
 )
 
-type PasswordHandler struct{}
+var hashes = make(map[int32]string)
+var lock = sync.RWMutex{}
+var jobCounter int32
+var hashStats atomic.Value
+var stopChan = make(chan os.Signal, 1)
+var stopping int32
+
+type HashStat struct {
+	Total   int32   `json:"total"`
+	Average float64 `json:"average"`
+}
+type HashHandler struct{}
+type StatsHandler struct{}
+
+func (hh HashHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//don't allow requests to process if the server is stopping
+	if atomic.LoadInt32(&stopping) == 1 {
+		http.Error(w, "Server is stopping.", http.StatusServiceUnavailable)
+		return
+	}
+
+	log.Println("Begining hash request.")
+
+	// only support POST and GET requests
+	// return http.StatusMethodNotAllowed if any other HTTP method
+	switch r.Method {
+	case http.MethodGet:
+		log.Println("Processing hash GET.")
+		//find the job id in the URL
+		urlParts := strings.Split(r.URL.Path, "/")
+		if len(urlParts) >= 3 && urlParts[2] != "" {
+			jid, err := strconv.Atoi(urlParts[2])
+			if err != nil {
+				http.Error(w, "Unable to process the supplied job id.", http.StatusBadRequest)
+				return
+			}
+
+			log.Printf("Looking for job id %d.", jid)
+
+			lock.RLock()
+			jidHash := hashes[int32(jid)]
+			lock.RUnlock()
+
+			if jidHash == "" {
+				http.Error(w, "Unable to find the specified job id.", http.StatusNotFound)
+				return
+			} else {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(jidHash))
+			}
+		} else {
+			http.Error(w, "No job id specified.", http.StatusBadRequest)
+			return
+		}
+	case http.MethodPost:
+		log.Println("Processing hash POST.")
+		hashStart := time.Now()
+		p := r.PostFormValue(passwordFormName)
+		if p != "" {
+			//get the next job id
+			jid := atomic.AddInt32(&jobCounter, 1)
+
+			//call after the handler completes
+			defer hashAndEncode(jid, p)
+
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(strconv.Itoa(int(jid))))
+
+			duration := time.Since(hashStart)
+			log.Printf("Hash request duration (nanoseconds): %s", strconv.FormatInt(duration.Nanoseconds(), 10))
+			updateStats(duration.Nanoseconds())
+		} else {
+			http.Error(w, "A value must be submitted.", http.StatusBadRequest)
+			return
+		}
+	default:
+		http.Error(w, "Only POST and GET requests are supported.", http.StatusMethodNotAllowed)
+	}
+
+	log.Println("Finished hash request.")
+}
+
+func (sh StatsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//don't allow requests to process if the server is stopping
+	if atomic.LoadInt32(&stopping) == 1 {
+		http.Error(w, "Server is stopping.", http.StatusServiceUnavailable)
+		return
+	}
+
+	log.Println("Begining stats request.")
+	// only support GET requests
+	// return http.StatusMethodNotAllowed if any other HTTP method
+	switch r.Method {
+	case http.MethodGet:
+		log.Println("Processing stats GET.")
+		//get the JSON to return to the client
+		hsj, err := json.Marshal(hashStats.Load().(*HashStat))
+		if err != nil {
+			http.Error(w, "Unable to generate stats.", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(hsj))
+	default:
+		http.Error(w, "Only GET requests are supported.", http.StatusMethodNotAllowed)
+	}
+
+	log.Println("Finished stats request.")
+}
 
 func main() {
-    // process command line flag for port number or default to 8080
-    portFlag := flag.Int("port", 8080, "Port number the server will listen on.")
-    
-    flag.Parse()
-    
-    log.Printf("Starting server on port %d", *portFlag)
-    err := http.ListenAndServe(fmt.Sprintf(":%d", *portFlag), new(PasswordHandler))
-    
-    if err != nil {
-        log.Fatal("An error was received during ListenAndServe: ", err)
-    }
+	// process command line flag for port number or default to 8080
+	portFlag := flag.Int("port", 8080, "Port number the server will listen on.")
+
+	flag.Parse()
+	//initialize the WaitGroup used when stopping the server
+	wg := sync.WaitGroup{}
+	//initialise the hashStats object before the server starts
+	hashStats.Store(&HashStat{Total: 0, Average: 0})
+
+	hashListener, err := net.Listen("tcp", fmt.Sprintf(":%d", *portFlag))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//handle various interrupts to stop the server gracefully
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-stopChan
+		//set global value used to indicate that the server is stopping
+		atomic.StoreInt32(&stopping, 1)
+		log.Println("Waiting for requests to finish.")
+		wg.Wait()
+		log.Println("Complete and shutting down.")
+		os.Exit(1)
+	}()
+
+	//register with and without the trailing slash to avoid redirect in the case of a POST request
+	http.Handle("/hash", HashHandler{})
+	http.Handle("/hash/", HashHandler{})
+	//only supporting GET requests, so the redirect when requested without the trailing slash is ok
+	//but registering both to avoid the unnecessary redirect
+	http.Handle("/stats", StatsHandler{})
+	http.Handle("/stats/", StatsHandler{})
+
+	server := &http.Server{
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		//simple callback function for managing the WaitGroup used during shutdown
+		ConnState: func(conn net.Conn, state http.ConnState) {
+			switch state {
+			case http.StateNew:
+				wg.Add(1)
+			case http.StateHijacked, http.StateClosed:
+				wg.Done()
+			}
+		},
+	}
+
+	log.Printf("Starting server on port %d", *portFlag)
+	err = server.Serve(hashListener)
+
+	if err != nil {
+		log.Fatal("An error was received during Serve: ", err)
+	}
 }
 
-func (ph PasswordHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    // only support POST requests
-    // return http.StatusMethodNotAllowed if any other HTTP method
-    switch r.Method {
-        case postMethod: 
-        	p := r.PostFormValue(passwordFormName)
-        	if p != "" {
-        	    // wait 5 seconds before responding
-        		time.Sleep(time.Duration(5)*time.Second)
-        		w.Write([]byte(encryptAndEncode(r.PostFormValue(passwordFormName))))
-        	} else {
-        	    // return http.StatusBadRequest if no value was submitted
-        	    w.WriteHeader(http.StatusBadRequest)
-        	    w.Write([]byte("A value must be submitted."))
-        	}
-        default: http.Error(w, "Only POST requests are supported.", http.StatusMethodNotAllowed)
-    }
+//private function to hash and encode the passed in data
+//store the computed value in the global hashes map for the supplied key
+func hashAndEncode(jid int32, data string) {
+	// wait 5 seconds before starting
+	time.Sleep(time.Duration(5) * time.Second)
+
+	log.Printf("Hashing and encoding: '%s'", data)
+
+	lock.Lock()
+
+	h := sha512.New()
+	h.Write([]byte(data))
+	cs := h.Sum(nil)
+
+	hashes[jid] = base64.StdEncoding.EncodeToString(cs)
+
+	lock.Unlock()
 }
 
-// private function to encrypt and encode
-// the supplied value
-func encryptAndEncode(data string) string {
-    log.Printf("Processing: '%s'\n", data)
-    
-    h := sha512.New()
-    h.Write([]byte(data))
-    cs := h.Sum(nil);
-    
-    return base64.StdEncoding.EncodeToString(cs)
+//update the stats total count and average values based on the most resent duration
+func updateStats(durationNano int64) {
+	hs := hashStats.Load().(*HashStat)
+	num := atomic.AddInt32(&hs.Total, 1)
+	durationMilli := float64(durationNano) / 1e6
+	avg := (((hs.Average * (float64(num) - 1)) + durationMilli) / float64(num))
+
+	log.Printf("Adding duration (ms) %s to stats. New average: %s", strconv.FormatFloat(durationMilli, 'f', 5, 64), strconv.FormatFloat(avg, 'f', 5, 64))
+	hashStats.Store(&HashStat{Total: num, Average: avg})
 }
--- src/github.com/mwaldtha/pwdsrv/password_server_test.go
+++ src/github.com/mwaldtha/pwdsrv/password_server_test.go
@@ -1,181 +1,333 @@
 package main
 
 import (
-    "io/ioutil"
-    "net/http"
-    "net/http/httptest"
-    "net/url"
-    "testing"
-    "time"
-    "strings"
-    "sync"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
+	"strings"
+	"sync"
+	"testing"
+	"time"
 )
 
 var testPasswords = []struct {
-    originalValue string
-    expectedValue string
+	originalValue string
+	expectedValue string
 }{
-    {"angryMonkey", "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="},
-    {"password", "sQnzu7wkTrgkQZF+0G1hi5AI3Qmzvv0bXgc5THBqi7mAsdd4Xll27ASbRt9fEyavWi6m0QP9B8lThf+rDKy8hg=="},
-    {"two words", "rbeOtyY+z9LrVIi2SX/tw4jmeucGQ80oUx1v0gz+8FpP0Jw21auG5p4yJL721e+vj3/dcQIDt0C9+H3fKu8SVA=="},
-    {"1234567890", "ErAyJqbYvpxujNXlXcbHkgyqo53xSquS1ePqk0DRyKTT0LjkMU8fbvExukvxzrkYarh8gBrw1clbG++4ztriuQ=="},
-    {" !~@#d{}[]WQS67*/?", "1CPHe9u49v+FXpdubV0IYYvNtkUn38l02Ijbw7Jn8JkSu54TgeVNDm4mWTAzm8iCedAdiWrlyWZ2diPycf67+Q=="},
-    {"Hello, 世界", "q5bnkSm2cCQbB/6S0TXdP5B6OKXUs2cn3J9HGgI+/jV+vIhKFuP1NvMYQ4nyB5gXf3IvisTGl+rHhfuZCHOLGw=="},
-    {"", "A value must be submitted."},
-    {"ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-", "KdRKHNcXx99I4FTpdQxqk4203FR1L8FwHDX0ovkuTB675g1c/BLMa78FSRguc6Ha/yIEF3+OxrFPnnSQqTML9A=="},
+	{"angryMonkey", "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="},
+	{"password", "sQnzu7wkTrgkQZF+0G1hi5AI3Qmzvv0bXgc5THBqi7mAsdd4Xll27ASbRt9fEyavWi6m0QP9B8lThf+rDKy8hg=="},
+	{"two words", "rbeOtyY+z9LrVIi2SX/tw4jmeucGQ80oUx1v0gz+8FpP0Jw21auG5p4yJL721e+vj3/dcQIDt0C9+H3fKu8SVA=="},
+	{"1234567890", "ErAyJqbYvpxujNXlXcbHkgyqo53xSquS1ePqk0DRyKTT0LjkMU8fbvExukvxzrkYarh8gBrw1clbG++4ztriuQ=="},
+	{" !~@#d{}[]WQS67*/?", "1CPHe9u49v+FXpdubV0IYYvNtkUn38l02Ijbw7Jn8JkSu54TgeVNDm4mWTAzm8iCedAdiWrlyWZ2diPycf67+Q=="},
+	{"Hello, 世界", "q5bnkSm2cCQbB/6S0TXdP5B6OKXUs2cn3J9HGgI+/jV+vIhKFuP1NvMYQ4nyB5gXf3IvisTGl+rHhfuZCHOLGw=="},
+	{"", "A value must be submitted."},
+	{"ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-ReallyLongPassword-", "KdRKHNcXx99I4FTpdQxqk4203FR1L8FwHDX0ovkuTB675g1c/BLMa78FSRguc6Ha/yIEF3+OxrFPnnSQqTML9A=="},
 }
 
-var httpMethods = []struct {
-    methodName string
-    expectedCode int
+var hashHandlerHttpMethods = []struct {
+	methodName   string
+	expectedCode int
 }{
-    {"POST", http.StatusBadRequest},
-    {"GET", http.StatusMethodNotAllowed},
-    {"PUT", http.StatusMethodNotAllowed},
-    {"DELETE", http.StatusMethodNotAllowed},
-    {"HEAD", http.StatusMethodNotAllowed},
+	{http.MethodPost, http.StatusBadRequest},
+	{http.MethodGet, http.StatusBadRequest},
+	{http.MethodPut, http.StatusMethodNotAllowed},
+	{http.MethodDelete, http.StatusMethodNotAllowed},
+	{http.MethodHead, http.StatusMethodNotAllowed},
+	{http.MethodPatch, http.StatusMethodNotAllowed},
 }
 
-// Test various passwords serially through the handler
-// not through a running server
-func TestPasswords(t *testing.T) {
-    handler := new(PasswordHandler)
-    
-    for _, testPassword := range testPasswords {
-	    recorder := httptest.NewRecorder()
-	    // create new request for each password in testPasswords
-	    // in this case the url specified below is not really used, so the value is irrelevant
-	    req, newReqErr := http.NewRequest("POST", "http://localhost/", strings.NewReader(passwordFormName + "=" + testPassword.originalValue))
-	    req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
-	    
-	    if newReqErr != nil {
-	        t.Error("An error occured while creating the request: ", newReqErr)
-	    }
-	    
-	    // process request through the handler
-	    handler.ServeHTTP(recorder, req)
-	    
-	    if recorder.Body.String() != testPassword.expectedValue {
-	        t.Errorf("Password test (%s): expected: %s, actual: %s", testPassword.originalValue, testPassword.expectedValue, recorder.Body.String())
-	    }
+var statsHandlerHttpMethods = []struct {
+	methodName   string
+	expectedCode int
+}{
+	{http.MethodPost, http.StatusMethodNotAllowed},
+	{http.MethodGet, http.StatusOK},
+	{http.MethodPut, http.StatusMethodNotAllowed},
+	{http.MethodDelete, http.StatusMethodNotAllowed},
+	{http.MethodHead, http.StatusMethodNotAllowed},
+	{http.MethodPatch, http.StatusMethodNotAllowed},
+}
+
+func TestHashHandlerPost(t *testing.T) {
+	handler := new(HashHandler)
+	hashStats.Store(&HashStat{Total: 0, Average: 0})
+	count := 0
+
+	for _, testPassword := range testPasswords {
+		recorder := httptest.NewRecorder()
+		// create new request for each password in testPasswords
+		// in this case the url specified below is not really used, so the value is irrelevant
+		req, newReqErr := http.NewRequest(http.MethodPost, "http://localhost/", strings.NewReader(passwordFormName+"="+testPassword.originalValue))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+
+		if newReqErr != nil {
+			t.Error("An error occured while creating the request: ", newReqErr)
+		}
+
+		// process request through the handler
+		handler.ServeHTTP(recorder, req)
+
+		body := strings.TrimSpace(recorder.Body.String())
+
+		if testPassword.originalValue != "" {
+			count++
+			jobInt, _ := strconv.Atoi(body)
+			if jobInt != count {
+				t.Errorf("HashHandlerPost test (%s): expected: 'Job ID: %d', actual: '%s'", testPassword.originalValue, count, body)
+			}
+		} else {
+			if body != testPassword.expectedValue {
+				t.Errorf("HashHandlerPost test (%s): expected: '%s', actual: '%s'", testPassword.originalValue, testPassword.expectedValue, body)
+			}
+		}
 	}
 }
 
-// Test how the handler responds to various HTTP methods
+func TestHashHandlerGet(t *testing.T) {
+	handler := new(HashHandler)
+	count := int32(0)
+
+	for _, testPassword := range testPasswords {
+		recorder := httptest.NewRecorder()
+		count++
+		hashes[count] = testPassword.expectedValue
+
+		// create new request for each password in testPasswords
+		req, newReqErr := http.NewRequest(http.MethodGet, "http://localhost/hash/"+strconv.FormatInt(int64(count), 10), nil)
+
+		if newReqErr != nil {
+			t.Error("An error occured while creating the request: ", newReqErr)
+		}
+
+		// process request through the handler
+		handler.ServeHTTP(recorder, req)
+
+		body := strings.TrimSpace(recorder.Body.String())
+
+		if body != testPassword.expectedValue {
+			t.Errorf("HashHandlerGet test (%s): expected: '%s', actual '%s'", testPassword.originalValue, testPassword.expectedValue, body)
+		}
+	}
+}
+
+func TestStatsHandlerGet(t *testing.T) {
+	handler := new(StatsHandler)
+
+	for x := 1; x <= 10; x++ {
+		average := float64(x) * 10.0
+		hashStats.Store(&HashStat{Total: int32(x), Average: average})
+		recorder := httptest.NewRecorder()
+
+		// create new request for each password in testPasswords
+		// in this case the url specified below is not really used, so the value is irrelevant
+		req, newReqErr := http.NewRequest(http.MethodGet, "http://localhost/", nil)
+
+		if newReqErr != nil {
+			t.Error("An error occured while creating the request: ", newReqErr)
+		}
+
+		// process request through the handler
+		handler.ServeHTTP(recorder, req)
+
+		body := strings.TrimSpace(recorder.Body.String())
+		bodyBytes := []byte(body)
+
+		var jsonMap map[string]interface{}
+
+		if err := json.Unmarshal(bodyBytes, &jsonMap); err != nil {
+			t.Errorf("Unable to unmarshal the response body: '%s'", body)
+		}
+
+		total := jsonMap["total"].(float64)
+		avg := jsonMap["average"].(float64)
+
+		if total != float64(x) && avg != float64(average) {
+			t.Errorf("StatsHandlerGet test - unexpected response: %s", body)
+		}
+	}
+}
+
+// Test how the HashHandler responds to various HTTP methods
 // without starting up a server or passing in a password
-func TestHandlerHTTPMethods(t *testing.T) {
-    handler := new(PasswordHandler)
-    
-    for _, method := range httpMethods {
-	    recorder := httptest.NewRecorder()
-	    // create new request for each HTTP method in httpMethods
-	    // in this case the url specified below is not really used, so the value is irrelevant
-	    req, newReqErr := http.NewRequest(method.methodName, "http://localhost/", nil)
-	    
-	    if newReqErr != nil {
-	        t.Error("An error occured while creating the request: ", newReqErr)
-	    }
-	    
-	    // process request through the handler
-	    handler.ServeHTTP(recorder, req)
-	    
-	    if recorder.Code != method.expectedCode {
-	        t.Errorf("HTTP %s request: expected: %d, actual: %d", method.methodName, method.expectedCode, recorder.Code)
-	    }
+func TestHashHandlerHTTPMethods(t *testing.T) {
+	handler := new(HashHandler)
+
+	for _, method := range hashHandlerHttpMethods {
+		recorder := httptest.NewRecorder()
+		// create new request for each HTTP method in httpMethods
+		// in this case the url specified below is not really used, so the value is irrelevant
+		req, newReqErr := http.NewRequest(method.methodName, "http://localhost/", nil)
+
+		if newReqErr != nil {
+			t.Error("An error occured while creating the request: ", newReqErr)
+		}
+
+		// process request through the handler
+		handler.ServeHTTP(recorder, req)
+
+		if recorder.Code != method.expectedCode {
+			t.Errorf("HTTP %s request: expected: %d, actual: %d", method.methodName, method.expectedCode, recorder.Code)
+		}
 	}
 }
 
-// Test how the handler responds to various HTTP methods
-// through a running server without passing in a password
-func TestServerHTTPMethods(t *testing.T) {
-    testServer := httptest.NewServer(new(PasswordHandler))
-    t.Logf("Running server at: %s", testServer.URL)
-    
-    // close the server after this test finishes
-    defer testServer.Close()
-    
-    client := &http.Client{}
-    
-	for _, method := range httpMethods {
-	    // create new request for each HTTP method in httpMethods
-	    req, newReqErr := http.NewRequest(method.methodName, testServer.URL, nil)
-	    
-	    if newReqErr != nil {
-	        t.Error("An error occured while creating the request: ", newReqErr)
-	    }
-	    
-	    // call client.Do() since HTTP method will differ
-	    actual, doErr := client.Do(req)
-	    
-	    if doErr != nil {
-	        t.Error("An error occured while submitting the request: ", doErr)
-	    }
-	    
-	    if actual.StatusCode != method.expectedCode {
-	        t.Errorf("HTTP %s request: expected: %d, actual: %d", method.methodName, method.expectedCode, actual.StatusCode)
-	    }
+// Test how the StatsHandler responds to various HTTP methods
+// without starting up a server
+func TestStatsHandlerHTTPMethods(t *testing.T) {
+	handler := new(StatsHandler)
+	hashStats.Store(&HashStat{Total: 0, Average: 0})
+
+	for _, method := range statsHandlerHttpMethods {
+		recorder := httptest.NewRecorder()
+		// create new request for each HTTP method in httpMethods
+		// in this case the url specified below is not really used, so the value is irrelevant
+		req, newReqErr := http.NewRequest(method.methodName, "http://localhost/", nil)
+
+		if newReqErr != nil {
+			t.Error("An error occured while creating the request: ", newReqErr)
+		}
+
+		// process request through the handler
+		handler.ServeHTTP(recorder, req)
+
+		if recorder.Code != method.expectedCode {
+			t.Errorf("HTTP %s request: expected: %d, actual: %d", method.methodName, method.expectedCode, recorder.Code)
+		}
 	}
 }
 
-// Test submitting passwords in parallel
-// through a running server
-func TestParallelRequests(t *testing.T) {
-    
-    // use a WaitGroup to wait for all goroutines in this test to finish
-    var wg sync.WaitGroup
-    
-    testServer := httptest.NewServer(new(PasswordHandler))
-    t.Logf("Running server at: %s", testServer.URL)
-    
-    // close the server after this test finishes
-    defer testServer.Close()
+//Test how the HashHandler responds to various HTTP methods
+//through a running server without passing in a password
+func TestHashHandlerServerHTTPMethods(t *testing.T) {
+	testServer := httptest.NewServer(new(HashHandler))
+	t.Logf("Running server at: %s", testServer.URL)
+
+	// close the server after this test finishes
+	defer testServer.Close()
+
+	client := &http.Client{}
+
+	for _, method := range hashHandlerHttpMethods {
+		// create new request for each HTTP method in httpMethods
+		req, newReqErr := http.NewRequest(method.methodName, testServer.URL, nil)
+
+		if newReqErr != nil {
+			t.Error("An error occured while creating the request: ", newReqErr)
+		}
+
+		// call client.Do() since HTTP method will differ
+		actual, doErr := client.Do(req)
+
+		if doErr != nil {
+			t.Error("An error occured while submitting the request: ", doErr)
+		}
+
+		if actual.StatusCode != method.expectedCode {
+			t.Errorf("HTTP %s request: expected: %d, actual: %d", method.methodName, method.expectedCode, actual.StatusCode)
+		}
+	}
+}
+
+//Test how the StatsHandler responds to various HTTP methods
+//through a running server without passing in a password
+func TestStatsHandlerServerHTTPMethods(t *testing.T) {
+	testServer := httptest.NewServer(new(StatsHandler))
+	t.Logf("Running server at: %s", testServer.URL)
+
+	// close the server after this test finishes
+	defer testServer.Close()
+
+	client := &http.Client{}
+
+	for _, method := range statsHandlerHttpMethods {
+		// create new request for each HTTP method in httpMethods
+		req, newReqErr := http.NewRequest(method.methodName, testServer.URL, nil)
+
+		if newReqErr != nil {
+			t.Error("An error occured while creating the request: ", newReqErr)
+		}
+
+		// call client.Do() since HTTP method will differ
+		actual, doErr := client.Do(req)
+
+		if doErr != nil {
+			t.Error("An error occured while submitting the request: ", doErr)
+		}
+
+		if actual.StatusCode != method.expectedCode {
+			t.Errorf("HTTP %s request: expected: %d, actual: %d", method.methodName, method.expectedCode, actual.StatusCode)
+		}
+	}
+}
+
+//Test how the HashHandler processes multiple parallel POST requests
+func TestParallelHashHandlerPostRequests(t *testing.T) {
+
+	// use a WaitGroup to wait for all goroutines in this test to finish
+	var wg sync.WaitGroup
+	hashStats.Store(&HashStat{Total: 0, Average: 0})
+
+	testServer := httptest.NewServer(new(HashHandler))
+	t.Logf("Running server at: %s", testServer.URL)
+
+	// close the server after this test finishes
+	defer testServer.Close()
 
 	// loop through testPasswords 10 times
 	for i := 0; i < 10; i++ {
-	    for _, testPassword := range testPasswords {
-	        wg.Add(1)
-	        t.Logf("%s - Submitting '%s'", time.Now().Format(time.StampNano), testPassword.originalValue)
-	        // run a seperate goroutine for each password request
-	        go doPostRequest(testServer.URL, testPassword.originalValue, testPassword.expectedValue, t, &wg)
-	   	}
-    }
+		for _, testPassword := range testPasswords {
+			wg.Add(1)
+			t.Logf("%s - Submitting '%s'", time.Now().Format(time.StampNano), testPassword.originalValue)
+			// run a separate goroutine for each password request
+			go doHashHandlerPostRequest(testServer.URL, testPassword.originalValue, testPassword.expectedValue, t, &wg)
+		}
+	}
 	wg.Wait()
-    
-    t.Log("Finshed parallel tests.")
+
+	t.Log("Finshed parallel HashHandler POST test.")
 }
 
-// private function to issue POST requests
+// private function to issue POST requests to the HashHandler
 // used by each goroutine in parallel testing
-func doPostRequest(serverURL string, orig string, expected string, t *testing.T, wg *sync.WaitGroup) {
-    
-    //tell the WaitGroup when this processing is done
-    defer wg.Done()
-    
-    client := &http.Client{}
-    
-    // create and assign password form value
-    v := url.Values{}
+func doHashHandlerPostRequest(serverURL string, orig string, expected string, t *testing.T, wg *sync.WaitGroup) {
+
+	//tell the WaitGroup when this processing is done
+	defer wg.Done()
+
+	client := &http.Client{}
+
+	// create and assign password form value
+	v := url.Values{}
 	v.Set(passwordFormName, orig)
-	
+
 	// issue POST request with password form value
 	postResponse, postErr := client.PostForm(serverURL, v)
-    
-    if postErr != nil {
-        t.Error("An error occured while submitting the request: ", postErr)
-    } else {
-        defer postResponse.Body.Close()
-        // read value returned in response body
-    	actual, readBodyErr := ioutil.ReadAll(postResponse.Body)
-    	
-    	if readBodyErr != nil {
-    	    t.Error("Unable to read the response body: ", readBodyErr)
-   		} else {
-   		    if string(actual) != expected {
-   		        t.Errorf("Password test (%s): expected: %s, actual: %s", orig, expected, string(actual))
-		    }
-   		}
-   	}
+
+	if postErr != nil {
+		t.Error("An error occured while submitting the request: ", postErr)
+	} else {
+		defer postResponse.Body.Close()
+		// read value returned in response body
+		actual, readBodyErr := ioutil.ReadAll(postResponse.Body)
+
+		if readBodyErr != nil {
+			t.Error("Unable to read the response body: ", readBodyErr)
+		} else {
+			body := strings.TrimSpace(string(actual))
+
+			if orig != "" {
+				_, err := strconv.Atoi(body)
+				if err != nil {
+					t.Errorf("HashHandlerPost test (%s): Unable to convert job id '%s'", orig, body)
+				}
+			} else {
+				if body != expected {
+					t.Errorf("HashHandlerPost test (%s): expected: '%s', actual: '%s'", orig, expected, body)
+				}
+			}
+		}
+	}
 }
-
-
-
